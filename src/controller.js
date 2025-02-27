37b526f294dd28a590c77d7de83f14201bb21d76
Jonathan Chiu <13129139+reppy-c@users.noreply.github.com>
2025-02-27 16:37:21 -0500

    add initial event listeners - created structure for all the DOM event listeners - added event listeners for the add TODO button and its close button

--- src/controller.js
+++ src/controller.js
@@ -7,7 +7,15 @@ import formatDate from "./utils/dateformatter.js";
 // Importing the menu icon to use in a TODO item, because the filepath within the template literal is not being caught by webpack
 import menuIconURL from "./icons/menu.svg";
 
+// Define constants for the different DOM elements
 const li_list = document.querySelector('#todo-list');
+const div_scrim = document.querySelector('#modal-scrim');
+const div_modal_create_todo = document.querySelector('#modal-create-todo');
+const div_modal_create_project = document.querySelector('#modal-create-project');
+const btn_add_todo = document.querySelector("#add-todo");
+const btn_close_todo = document.querySelector("#close-modal-create-todo");
+
+// Other variables
 let currentProject;
 
 // Function to create HTML out of a todo object using template literals
@@ -25,7 +33,29 @@ const itemHTML = (todo) => `
     <button class="item-menu"><img src="${menuIconURL}" /></button>
 `;
 
-// Attach event listener to Add TODO button
+// Attach all the event listeners
+function addEventListeners() {
+    
+    // Add TODO button
+    btn_add_todo.addEventListener("click", () => {
+        div_scrim.classList.add('show');
+        div_modal_create_todo.classList.add('show');
+    });
+
+    // Close TODO button
+    btn_close_todo.addEventListener("click", () => {
+        div_scrim.classList.remove('show');
+        div_modal_create_todo.classList.remove('show');
+    });
+
+    // Scrim 
+    div_scrim.addEventListener("click", () => {
+        div_scrim.classList.remove('show');
+        div_modal_create_todo.classList.remove('show');
+        // div_modal_create_project.classList.remove('show');
+    });
+
+}
 
 // This is the one function that will be called by index.js
 export function initializeController() {
@@ -46,6 +76,9 @@ export function initializeController() {
 
             // Append the li to the actual DOM
             li_list.append(itemToCreate);
+
+            // Attach event listeners for checkbox and the menu
+
         });
     }
 
@@ -57,4 +90,5 @@ export function initializeController() {
     
     // Display items
     displayItems();
+    addEventListeners();
 }
